ab82acef7b8dabf6510acad7d1d69c1dc1d2ebcd
김성현 <kimseonghyun@gimseonghyeon-ui-MacBookPro.local>
2024-11-24 17:04:59 +0900

    feat: add temporary Header & Footer component

---
+++ src/app/_component/Footer.tsx
@@ -0,0 +1,36 @@
+type FooterProps = {};
+
+const Footer: React.FC<FooterProps> = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-[130px] bg-[#444655] text-white text-[14px] mt-auto">
+      <div className="w-[1024px] mb-[-15px]">
+        <div className="flex flex-row start-1 gap-1 my-[-2px]">
+          <img
+            src={`/assets/images/team-logo.png`}
+            alt="Team Logo"
+            className="h-8 w-8 relative top-[-5px]"
+          />
+          <p>Pretzel</p>
+        </div>
+        <div className="flex flex-row start-1 gap-3 my-[-2px]">
+          <img
+            src={`/assets/images/email.png`}
+            alt="Team Email"
+            className="h-5 w-5 relative top-[-2px] left-[5px] mr-[4px]"
+          />
+          <p>pretzelll9999@gmail.com</p>
+        </div>
+        <div className="flex flex-row start-1 gap-3 my-[10px]">
+          <img
+            src={`/assets/images/tool.png`}
+            alt="Team Info"
+            className="h-5 w-5 relative top-[-2px] left-[5px] mr-[4px]"
+          />
+          <p>andy</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/_component/Header.tsx
@@ -0,0 +1,91 @@
+"use client"
+import React, { useState } from "react";
+import Link from "next/link";
+
+
+const Header: React.FC = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isAlarmModalOpen, setAlarmModalOpen] = useState(false);
+  const userId:Number = 1;
+  const profileImage:any = null;
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
+  const toggleAlarmModal = () => {
+    setAlarmModalOpen(!isAlarmModalOpen);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const closeAlarmModal = () => {
+    setAlarmModalOpen(false);
+  };
+
+  return (
+    <header className="fixed w-full bg-white z-50">
+      <div className="flex items-center justify-between w-full max-w-[1024px] h-[80px] mx-auto px-4 md:px-0 relative">
+        <div className="flex items-center space-x-4 md:space-x-20">
+          <Link href="/" className="flex items-center space-x-2">
+            <img
+              src={`/assets/images/logo.png`}
+              alt="Logo"
+              className="h-8 w-8"
+            />
+            <span className="text-lg md:text-xl font-bold text-[#6D81D5]">
+              Dreamketcher
+            </span>
+          </Link>
+        </div>
+
+        <div className="flex justify-end w-full max-w-[670px]">
+          <div className="flex flex-row w-[380px]">
+            <input
+                type="text"
+                placeholder="Search..."
+                className="w-full h-[44px] p-2 border-b border-black"
+              />
+            <button className="">
+              <img
+                src={`/assets/images/magnifier.png`}
+                alt="Search"
+                className="w-[15px] cursor-pointer object-scale-down"
+              />
+            </button>
+          </div>
+        </div>
+        <div className="relative flex">
+          {userId !== 0 ? (
+            <>
+            <Link
+              href="/login"
+              className="px-4 py-2"
+            >
+              마이페이지
+            </Link>
+              {/* 알림 아이콘 숨김 처리 */}
+              <Link
+              href="/login"
+              className="px-4 py-2"
+            >
+              로그인
+            </Link>
+            </>
+          ) : (
+            <Link
+              href="/login"
+              className="px-4 py-2"
+            >
+              로그아웃
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
