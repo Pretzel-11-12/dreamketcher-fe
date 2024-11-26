071b3e6f7795d7bd76aad4db7dc60d6add7580b4
이소희 <isohui@isohuis-Mac-mini.local>
2024-11-27 04:41:52 +0900

    [FE] feat: Add Button Component

--- src/app/(afterLogin)/index/page.tsx
+++ src/app/(afterLogin)/index/page.tsx
@@ -1,3 +1,31 @@
+// "use client";
+import Button from "@/app/_component/Button";
+import Link from "next/link";
+
 export default function Page() {
-    return <div>Page Content</div>;
-  }
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Link href={"/"}>
+        <Button size="S" variant="bg-blue">
+          Home
+        </Button>
+      </Link>
+
+      <Button size="S" variant="bg-blue" disabled>
+        Button
+      </Button>
+
+      <Button
+        size="S"
+        variant="bg-gray"
+        containerStyles={"rounded-none py-3 px-5"}
+      >
+        Custom Button
+      </Button>
+
+      <Button size="S" variant="bg-transparent">
+        Ghost Button
+      </Button>
+    </div>
+  );
+}
