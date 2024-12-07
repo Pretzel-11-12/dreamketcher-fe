019c4a3122983f98c832e09df957460f1cac1a9c
seorinnn <srgongju@naver.com>
2024-12-07 13:22:10 +0900

    feat : 헤더에 로그인 버튼 클릭 시 로그인 기능 추가

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -1,13 +1,13 @@
-"use client"
-import React, { useState } from "react";
-import Link from "next/link";
-
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
 
 const Header: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [isAlarmModalOpen, setAlarmModalOpen] = useState(false);
-  const userId:Number = 1;
-  const profileImage:any = null;
+  const userId: Number = 1;
+  const profileImage: any = null;
 
   const toggleModal = () => {
     setModalOpen(!isModalOpen);
@@ -26,59 +26,55 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="fixed w-full bg-white z-50">
-      <div className="flex items-center justify-between w-full max-w-[1024px] h-[80px] mx-auto px-4 md:px-0 relative">
-        <div className="flex items-center space-x-4 md:space-x-20">
-          <Link href="/" className="flex items-center space-x-2">
+    <header className='fixed w-full bg-white z-50'>
+      <div className='flex items-center justify-between w-full max-w-[1024px] h-[80px] mx-auto px-4 md:px-0 relative'>
+        <div className='flex items-center space-x-4 md:space-x-20'>
+          <Link href='/' className='flex items-center space-x-2'>
             <img
               src={`/assets/images/logo.png`}
-              alt="Logo"
-              className="h-8 w-8"
+              alt='Logo'
+              className='h-8 w-8'
             />
-            <span className="text-lg md:text-xl font-bold text-[#6D81D5]">
+            <span className='text-lg md:text-xl font-bold text-[#6D81D5]'>
               Dreamketcher
             </span>
           </Link>
         </div>
 
-        <div className="flex justify-end w-full max-w-[670px]">
-          <div className="flex flex-row w-[380px]">
+        <div className='flex justify-end w-full max-w-[670px]'>
+          <div className='flex flex-row w-[380px]'>
             <input
-                type="text"
-                placeholder="Search..."
-                className="w-full h-[44px] p-2 border-b border-black"
-              />
-            <button className="">
+              type='text'
+              placeholder='Search...'
+              className='w-full h-[44px] p-2 border-b border-black'
+            />
+            <button className=''>
               <img
                 src={`/assets/images/magnifier.png`}
-                alt="Search"
-                className="w-[15px] cursor-pointer object-scale-down"
+                alt='Search'
+                className='w-[15px] cursor-pointer object-scale-down'
               />
             </button>
           </div>
         </div>
-        <div className="relative flex text-black">
+        <div className='relative flex text-black'>
           {userId !== 0 ? (
             <>
-            <Link
-              href="/login"
-              className="px-4 py-2"
-            >
-              마이페이지
-            </Link>
+              <Link href='/mypage' className='px-4 py-2'>
+                마이페이지
+              </Link>
               {/* 알림 아이콘 숨김 처리 */}
-              <Link
-              href="/login"
-              className="px-4 py-2"
-            >
-              로그인
-            </Link>
+              {/* <Link
+                href='/api/auth/signin'
+                className='px-4 py-2'
+                onClick={() => signIn('google')}
+              >
+                로그인
+              </Link> */}
+              <button onClick={() => signIn('google')}>로그인</button>
             </>
           ) : (
-            <Link
-              href="/login"
-              className="px-4 py-2"
-            >
+            <Link href='/login' className='px-4 py-2'>
               로그아웃
             </Link>
           )}
