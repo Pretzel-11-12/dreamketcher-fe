c6b0bc96ba5be241e716082f651f5528f97b5aaf
seorinnn <srgongju@naver.com>
2024-12-08 20:09:39 +0900

    chore : next.config.ts 파일에 svgr 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+const withSvgr = require('next-svgr');
+
+module.exports = withSvgr({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
