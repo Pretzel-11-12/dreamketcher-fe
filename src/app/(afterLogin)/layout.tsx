2620fafb67ebf9bf6344206e1c9bd200b3f502ce
이소희 <isohui@isohuis-Mac-mini.local>
2024-12-10 03:15:12 +0900

    [FE] feat: Add Detail Page

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -2,14 +2,8 @@ import { ReactNode } from "react";
 import Header from "@/app/_component/Header";
 import Footer from "@/app/_component/Footer";
 
-type Props = { children: ReactNode };  // modal 제거
+type Props = { children: ReactNode }; // modal 제거
 
 export default function AfterLoginLayout({ children }: Props) {
-  return (
-    <div>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
+  return <div>{children}</div>;
 }
---
+++ src/app/(afterLogin)/webtoon/_component/Comment.tsx
@@ -0,0 +1,39 @@
+import Button from "@/app/_component/Button";
+
+export interface CommentInfo {
+  id: string;
+  name: string;
+  description: string;
+  timestamp: number;
+  like: number;
+}
+export default function Comment({
+  id,
+  name,
+  description,
+  timestamp,
+  like,
+}: CommentInfo) {
+  const time = new Date(timestamp).toLocaleString();
+  return (
+    <div className="flex flex-col p-3 border rounded-md gap-1">
+      <div className="flex w-full">
+        <span className="mdi mdi-account-circle text-gray-500/70 text-5xl"></span>
+        <div className="flex flex-col gap-1 w-full">
+          <div className="flex w-full justify-between">
+            <div className="font-bold">{name}</div>
+            <div>{time}</div>
+          </div>
+          <div className="font-thin">{description}</div>
+        </div>
+      </div>
+
+      <Button size="S" variant="bg-transparent">
+        <div className="flex gap-0.5">
+          <span className="mdi mdi-thumb-up-outline text-sm"></span>
+          <div>{like}</div>
+        </div>
+      </Button>
+    </div>
+  );
+}
