60cca9316706ec0688291d130b0373db48400789
이소희 <isohui@isohuis-Mac-mini.local>
2024-12-12 01:45:54 +0900

    [FE] feat: Episode Detail Page

--- src/app/(afterLogin)/webtoon/detail/page.tsx
+++ src/app/(afterLogin)/webtoon/detail/page.tsx
@@ -1,12 +1,43 @@
-import User, { UserInfo } from "../_component/User";
-import Comment, { CommentInfo } from "../_component/Comment";
+import WriterInfoItem, { UserInfo } from "./_component/WriterInfoItem";
+import CommentListItem, { CommentInfo } from "./_component/CommentListItem";
+import EpisodeButtonGroup from "./_component/EpisodeButtonGroup";
 
-interface DetailUnit {
-  title: string;
-  images: string[];
-  detail: { like: number; interest: number; star: number };
-  writerInfo: UserInfo;
-}
+const commnets = [
+  {
+    id: 1,
+    name: "comment 1",
+    description:
+      "문제를 수 브라운관이, 다양하는, 그 관한다, 하는 등 이룰 하에요. 친하던 다시 꺼내는 숙명의 것 촉촉이 하면서 어떠할 등, 및 일컬어지느냐. 개성 이야기에 시설이 하거나 씰룩거리려 그녀를 이래 기성세대를 늘어날까. 모습에 대가부장적 축하에 농사철이 행복해함, 하나를 2025년 선장을 하나다 복합하다. 아니는 능행을 분수의 범으로 이용자의 깨닫아 목표를 돌리다.",
+    like: 1,
+    timestamp: 1730766637666,
+  },
+  {
+    id: 2,
+    name: "comment 2",
+    description:
+      "문제를 수 브라운관이, 다양하는, 그 관한다, 하는 등 이룰 하에요. 친하던 다시 꺼내는 숙명의 것 촉촉이 하면서 어떠할 등, 및 일컬어지느냐. 개성 이야기에 시설이 하거나 씰룩거리려 그녀를 이래 기성세대를 늘어날까. 모습에 대가부장적 축하에 농사철이 행복해함, 하나를 2025년 선장을 하나다 복합하다. 아니는 능행을 분수의 범으로 이용자의 깨닫아 목표를 돌리다.",
+    like: 1,
+    timestamp: 1730766637666,
+  },
+  {
+    id: 3,
+    name: "comment 3",
+    description:
+      "문제를 수 브라운관이, 다양하는, 그 관한다, 하는 등 이룰 하에요. 친하던 다시 꺼내는 숙명의 것 촉촉이 하면서 어떠할 등, 및 일컬어지느냐. 개성 이야기에 시설이 하거나 씰룩거리려 그녀를 이래 기성세대를 늘어날까. 모습에 대가부장적 축하에 농사철이 행복해함, 하나를 2025년 선장을 하나다 복합하다. 아니는 능행을 분수의 범으로 이용자의 깨닫아 목표를 돌리다.",
+    like: 1,
+    timestamp: 1730766637666,
+  },
+];
+const episodeData = {
+  title: "",
+  images: [],
+  writerInfo: {
+    name: "바크베",
+    description:
+      "문제를 수 브라운관이, 다양하는, 그 관한다, 하는 등 이룰 하에요. 친하던 다시 꺼내는 숙명의 것 촉촉이 하면서 어떠할 등, 및 일컬어지느냐. 개성 이야기에 시설이 하거나 씰룩거리려 그녀를 이래 기성세대를 늘어날까. 모습에 대가부장적 축하에 농사철이 행복해함, 하나를 2025년 선장을 하나다 복합하다. 아니는 능행을 분수의 범으로 이용자의 깨닫아 목표를 돌리다.",
+  },
+  detail: { like: 38, interest: 1147, star: 4.85 },
+};
 
 export default async function Detail({
   params,
@@ -17,110 +48,22 @@ export default async function Detail({
 }) {
   const { titleId, no } = await searchParams;
 
-  let item: DetailUnit = {
-    title: "Loading...",
-    images: [],
-    detail: { like: 0, interest: 0, star: 0 },
-    writerInfo: { name: "", description: "" },
-  };
-  let comments: CommentInfo[] = [];
-
-  try {
-    const response: { data: any } = await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            title: titleId,
-            images: [],
-            writerInfo: {
-              name: "바크베",
-              description: `그룹명 'aespa (에스파)'는 ‘아바타 X 익스피리언스 (Avatar X Experience)’를 표현한 ‘æ’와 양면이라는 뜻의 영단어
-              ‘aspect’를 결합해 만든 이름이다. 그룹명을 æspa로 표기할 때도 있다.`,
-            },
-            detail: { like: 38, interest: 1147, star: 4.85 },
-          },
-        });
-      }, 300);
-    });
-
-    const commnets: { data: any } = await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          data: [
-            {
-              id: 1,
-              name: "comment 1",
-              description: `comment 1`,
-              like: 1,
-              timestamp: 1730766637666,
-            },
-            {
-              id: 2,
-              name: "comment 2",
-              description: `그룹명 'aespa (에스파)'는 ‘아바타 X 익스피리언스 (Avatar X Experience)’를 표현한 ‘æ’와 양면이라는 뜻의 영단어 ‘aspect’를 결합해 만든 이름이다. 그룹명을 æspa로 표기할 때도 있다.`,
-              like: 1,
-              timestamp: 1730766637666,
-            },
-            {
-              id: 3,
-              name: "comment 3",
-              description: `comment 3`,
-              like: 1,
-              timestamp: 1730766637666,
-            },
-          ],
-        });
-      }, 300);
-    });
-
-    item = await response.data;
-    comments = await commnets.data;
-  } catch (error) {
-    console.error("Error fetching webtoon detail:", error as Error);
-  }
-
   return (
     <div className="flex flex-col gap-16 w-[800px] items-center justify-center text-md">
       <div className="bg-[#D9D9D9] h-[2000px] w-full"></div>
 
-      <div className="bg-[#fffff] border rounded-md flex w-full h-[100px] hover:cursor-pointer">
-        <div className="w-full h-full flex flex-col items-center justify-center border-r p-4 hover:bg-slate-300/20 gap-0.5">
-          <span className="mdi mdi-thumb-up-outline text-xl"></span>
-          <div>좋아요</div>
-          <div>{item.detail?.like}</div>
-        </div>
-
-        <div className="w-full h-full flex flex-col items-center justify-center border-r p-4 hover:bg-slate-300/20 gap-0.5">
-          <span className="mdi mdi-plus text-xl"></span>
-          <div>관심웹툰</div>
-          <div>{item.detail?.interest}</div>
-        </div>
-
-        <div className="w-full h-full flex flex-col items-center justify-center border-r p-4 hover:bg-slate-300/20 gap-0.5">
-          <span className="mdi mdi-star-outline text-xl"></span>
-          <div>별점주기</div>
-          <div>{item.detail?.star}</div>
-        </div>
-        <div className="w-full h-full flex flex-col items-center justify-center border-r p-4 hover:bg-slate-300/20 gap-0.5">
-          <span className="mdi mdi-bell-alert text-xl"></span>
-          <div>신고하기</div>
-        </div>
-        <div className="w-full h-full flex flex-col items-center justify-center p-4 hover:bg-slate-300/20 gap-0.5">
-          <span className="mdi mdi-export-variant text-xl"></span>
-          <div>공유하기</div>
-        </div>
-      </div>
-
-      <div className="h-full w-full flex flex-col gap-5">
-        <div>작가의 말</div>
-        <User
-          name={item.writerInfo?.name}
-          description={item.writerInfo?.description}
+      <EpisodeButtonGroup />
+      <div className="h-full w-full flex flex-col pb-32">
+        <div className="text-lg font-medium pb-2">작가의 말</div>
+        <WriterInfoItem
+          name={episodeData.writerInfo?.name}
+          description={episodeData.writerInfo?.description}
         />
-        <div>댓글</div>
+
+        <div className="text-lg font-medium pt-10 pb-2">댓글</div>
         <div className="flex flex-col gap-2">
-          {comments.map((comment) => (
-            <Comment
+          {commnets.map((comment) => (
+            <CommentListItem
               id={comment.id}
               name={comment.name}
               description={comment.description}
