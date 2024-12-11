91a7a0c199744cdd1906a1dc8b0191689bfad83b
seorinnn <srgongju@naver.com>
2024-12-11 14:39:58 +0900

    chore : next.config.ts 프록시 설정 추가 및 nextConfig 통합

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
 const withSvgr = require('next-svgr');
 
-module.exports = withSvgr({
-  reactStrictMode: true,
-});
+const nextConfig: NextConfig = {
+  reactStrictMode: true, // React의 엄격 모드 활성화
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 클라이언트가 요청하는 경로
+        destination: 'http://localhost:8080/api/:path*', // 프록시할 백엔드 경로
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = withSvgr(nextConfig);
