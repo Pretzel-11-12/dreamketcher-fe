bbcea68eb4a28413b7a9d4590fdd28dc9563f388
이소희 <isohui@isohuis-Mac-mini.local>
2024-12-14 23:10:59 +0900

    [FE] feat: Detail 페이지 Quick Menu & Header, Footer

--- src/app/webtoon/detail/layout.tsx
+++ src/app/webtoon/detail/layout.tsx
@@ -1,11 +1,23 @@
 import { ReactNode, useEffect, useState } from "react";
+import EpisodeHeader from "./_component/EpisodeHeader";
+import EpisodeFooter from "./_component/EpisodeFooter";
+import QuickMenu from "./_component/QuickMenu";
 
 type Props = { children: ReactNode };
 
 export default function DetailLayout({ children }: Props) {
   return (
-    <div className="pt-[100px] pb-10 w-full flex flex-col items-center">
-      {children}
-    </div>
+    <>
+      <div className="pt-[80px] pb-10">
+        <EpisodeHeader />
+        <QuickMenu />
+
+        <div className="pt-[100px] w-full flex flex-col items-center">
+          {children}
+        </div>
+
+        <EpisodeFooter />
+      </div>
+    </>
   );
 }
