4ee50d5b90976b552dd3d3e68acaf8986c04b917
seorinnn <srgongju@naver.com>
2024-12-14 23:59:12 +0900

    feat : 로그인 후 사용자 정보 호출 및 전역 변수 저장 기능 구현

--- src/app/(beforeLogin)/login/google/page.tsx
+++ src/app/(beforeLogin)/login/google/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { useEffect } from 'react';
+import useAuthStore from '@/app/store/authStore';
+import { fetchUserInfo } from '@/app/api/auth';
 
 const GoogleCallbackPage: React.FC = () => {
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const setUserInfo = useAuthStore((state) => state.setUserInfo);
+
   useEffect(() => {
     const handleGoogleCallback = async () => {
       const authCode = new URLSearchParams(window.location.search).get('code');
@@ -25,13 +30,24 @@ const GoogleCallbackPage: React.FC = () => {
         }
 
         const data = await response.json();
-
         const accessToken = data.accessToken;
+
         if (!accessToken) {
           throw new Error('Access token is missing in the response.');
         }
+        setAccessToken(accessToken);
         localStorage.setItem('accessToken', accessToken);
+
+        const userInfo = await fetchUserInfo(accessToken);
+        setUserInfo({
+          id: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          imageUrl: userInfo.imageUrl,
+        });
+
         alert('로그인에 성공했습니다.');
+
         window.location.href = '/main';
       } catch (error) {
         console.error('Error during authentication:', error);
@@ -39,7 +55,7 @@ const GoogleCallbackPage: React.FC = () => {
     };
 
     handleGoogleCallback();
-  }, []);
+  }, [[setAccessToken, setUserInfo]]);
 
   return (
     <div className='flex items-center justify-center h-screen'>
