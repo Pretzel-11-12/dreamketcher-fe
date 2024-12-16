5052ff02ff6e04920a47dfd8c65461b2cab858d3
seorinnn <srgongju@naver.com>
2024-12-16 09:48:02 +0900

    feat : 마이페이지 헤더 수정

--- src/app/_component/MypageHeader.tsx
+++ src/app/_component/MypageHeader.tsx
@@ -1,21 +1,74 @@
-import Link from 'next/link';
+'use client';
 
-const MypageHeader = () => {
+import React from 'react';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+
+const MypageHeader: React.FC = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isMypageActive = pathname === '/mypage';
+  const isStorageActive = pathname === '/mypage/storage';
+
   return (
-    <header
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        paddingTop: '100px',
-        backgroundColor: '#f5f5f5',
-        gap: '20px',
-      }}
-    >
-      <Link href='/mypage'>내 정보</Link>
-      <Link href='/mypage/info'>정보 변경</Link>
-      <Link href='/mypage/favorites'>관심 웹툰</Link>
-      <Link href='/mypage/recent'>방금 본 웹툰</Link>
-    </header>
+    <aside className='w-[277px] h-full flex flex-col items-start pr-[20px] border-r border-r-line mt-24'>
+      <div
+        className={`flex items-center w-full h-[50px] rounded-md px-4 py-2 ${
+          isMypageActive ? 'bg-[#E4EBFF]' : ''
+        }`}
+      >
+        <Image
+          src={
+            isMypageActive
+              ? '/assets/images/activeTaskSquare.svg'
+              : '/assets/images/inactiveTaskSquare.svg'
+          }
+          alt='Task Icon'
+          width={20}
+          height={20}
+          className='mr-2'
+        />
+        <button
+          className={`text-[16px] font-medium ${
+            isMypageActive
+              ? 'text-[#2E4072]'
+              : 'text-gray-800 hover:text-blue-600'
+          }`}
+          onClick={() => router.push('/mypage')}
+        >
+          마이 페이지
+        </button>
+      </div>
+
+      <div
+        className={`flex items-center w-full h-[50px] rounded-md px-4 py-2 ${
+          isStorageActive ? 'bg-[#E4EBFF]' : ''
+        }`}
+      >
+        <Image
+          src={
+            isStorageActive
+              ? '/assets/images/activeArchive.svg'
+              : '/assets/images/inactiveArchive.svg'
+          }
+          alt='Archive Icon'
+          width={20}
+          height={20}
+          className='mr-2'
+        />
+        <button
+          className={`text-[16px] font-medium ${
+            isStorageActive
+              ? 'text-[#2E4072]'
+              : 'text-gray-800 hover:text-blue-600'
+          }`}
+          onClick={() => router.push('/mypage/storage')}
+        >
+          보관함
+        </button>
+      </div>
+    </aside>
   );
 };
 
