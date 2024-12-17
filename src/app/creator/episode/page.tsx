a7e32d4874ede674bec6666f7d38e7290bc94531
이소희 <isohui@isohuis-Mac-mini.local>
2024-12-17 23:23:17 +0900

    test: git rule

--- src/app/creator/episode/page.tsx
+++ src/app/creator/episode/page.tsx
@@ -11,7 +11,7 @@ export default function CreatorMain() {
       <div className="flex flex-col px-4">
         <div className="flex items-center justify-between px-2 pt-2">
           <span className="text-lg font-semibold">
-            별종의 세계 - 회차 리스트
+            별종의 세계 - 회차 리스트.
           </span>
           <Link
             className="w-[125px] h-[39px] flex items-center justify-center bg-brand-yellow text-white rounded-[5px]"
