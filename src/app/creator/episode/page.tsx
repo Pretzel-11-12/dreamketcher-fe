1867eb97d2bb379ec04756431b7b6499a485ee1b
이소희 <isohui@isohuis-Mac-mini.local>
2024-12-21 16:21:07 +0900

    feat: Global component: Modal 개발

--- src/app/creator/episode/page.tsx
+++ src/app/creator/episode/page.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import SeriesCategorySelector from "../_component/SeriesCategorySelector";
-import EpisodeSideBar from "../_component/EpisodeSideBar";
-import EpisodeList from "../_component/EpisodeList";
-import CategoryTab from "@/app/_component/CategoryTab";
+import Link from 'next/link';
+import SeriesCategorySelector from '../_component/SeriesCategorySelector';
+import EpisodeSideBar from '../_component/EpisodeSideBar';
+import EpisodeList from '../_component/EpisodeList';
+import CategoryTab from '@/app/_component/CategoryTab';
 
 export default function CreatorMain() {
   return (
@@ -11,7 +11,7 @@ export default function CreatorMain() {
       <div className="flex flex-col px-4">
         <div className="flex items-center justify-between px-2 pt-2">
           <span className="text-lg font-semibold">
-            별종의 세계 - 회차 리스트.
+            별종의 세계 - 회차 리스트
           </span>
           <Link
             className="w-[125px] h-[39px] flex items-center justify-center bg-brand-yellow text-white rounded-[5px]"
@@ -23,21 +23,21 @@ export default function CreatorMain() {
 
         <CategoryTab
           items={[
-            { id: "1", label: "회차", subLabel: "(0)" },
-            { id: "2", label: "공지", subLabel: "(0)" },
+            { id: '1', label: '회차', subLabel: '(0)' },
+            { id: '2', label: '공지', subLabel: '(0)' },
           ]}
-          selectedId={"1"}
+          selectedId={'1'}
         />
         <div className="w-full h-0.1 border-b" />
 
         <CategoryTab
           items={[
-            { id: "1", label: "발행", subLabel: "(0)" },
-            { id: "2", label: "임시저장", subLabel: "(0)" },
-            { id: "3", label: "예약글", subLabel: "(0)" },
-            { id: "4", label: "휴지통", subLabel: "(0)" },
+            { id: '1', label: '발행', subLabel: '(0)' },
+            { id: '2', label: '임시저장', subLabel: '(0)' },
+            { id: '3', label: '예약글', subLabel: '(0)' },
+            { id: '4', label: '휴지통', subLabel: '(0)' },
           ]}
-          selectedId={"1"}
+          selectedId={'1'}
         />
         <div className="w-full h-0.1 border-b" />
         <EpisodeList />
--- src/app/creator/series/new/page.tsx
+++ src/app/creator/series/new/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import { useSearchParams } from "next/navigation";
-import SeriesForm from "../../_component/SeriesForm";
-import EpisodeSideBar from "../../_component/EpisodeSideBar";
+import { useSearchParams } from 'next/navigation';
+import SeriesForm from '../../_component/SeriesForm';
+import EpisodeSideBar from '../../_component/EpisodeSideBar';
 
 export default function SeriesNew() {
   const searchParams = useSearchParams();
-  const seriesId = searchParams.get("seriesId");
+  const seriesId = searchParams.get('seriesId');
   const isExist = !!seriesId;
 
   return (
     <div
       className={`grid grid-cols-[${
-        isExist ? "auto_1fr" : "1fr"
+        isExist ? 'auto_1fr' : '1fr'
       }] mt-[80px] w-full h-full`}
     >
       {isExist && <EpisodeSideBar />}
 
       <div className="flex flex-col w-full px-8">
         <div className="text-xl font-semibold py-4 border-b">
-          {isExist ? "별종의 세계 - 작품 수정" : "새 작품 등록"}
+          {isExist ? '별종의 세계 - 작품 수정' : '새 작품 등록'}
         </div>
         <div className="py-8">
           <SeriesForm />
