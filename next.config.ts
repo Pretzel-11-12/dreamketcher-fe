3b1bb110634a070ded54303e1214c8cb7b5e0ecb
seorinnn <srgongju@naver.com>
2024-12-22 21:24:30 +0900

    delete : 주석 삭제

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*', // 클라이언트가 요청하는 경로
         destination: 'http://43.200.18.88:8080/api/:path*', // 프록시할 백엔드 경로
-      }, //~~~~43.200.18.88:8080/api/v1/~~~~
+      },
     ];
   },
 };
