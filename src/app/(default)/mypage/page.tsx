a3287a5ec7e262e101b4d04c2a376756822d9a19
seorinnn <srgongju@naver.com>
2024-12-23 09:51:44 +0900

    feat : 임시 prettier 설정 적용 및 마이페이지 프로필에 ClipboardButton 추가

--- src/app/(default)/mypage/page.tsx
+++ src/app/(default)/mypage/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 
 import MyWork from './_component/MyWork';
 import { useRouter } from 'next/navigation';
+import ClipboardButton from '@/app/_component/ClipboardButton';
 
 export default function Mypage() {
   const router = useRouter();
@@ -31,6 +32,8 @@ export default function Mypage() {
     )}&bio=${encodeURIComponent(tempUserInfo.bio)}`;
     router.push(`/mypage/edit${query}`);
   };
+
+  const emailText = `rhdiddl@gmail.com${email}`;
 
   useEffect(() => {
     const handleFetchUserInfo = async () => {
@@ -56,39 +59,40 @@ export default function Mypage() {
   }, [setUserInfo]);
 
   return (
-    <div className='min-h-screen flex flex-col pr-[20px] border-r border-r-line'>
-      <div className='max-w-4xl bg-white mt-[70px] flex'>
+    <div className="min-h-screen flex flex-col pr-[20px] border-r border-r-line">
+      <div className="max-w-4xl bg-white mt-[70px] flex">
         <img
-          className='w-[70px] h-[70px] rounded-full mr-6'
+          className="w-[70px] h-[70px] rounded-full mr-6"
           src={imageUrl || '/assets/images/profile1.png'}
-          alt='프로필'
+          alt="프로필"
         />
 
-        <div className='ml-[4px]'>
-          <p className='text-[18px] font-medium text-gray-800 mt-[8px]'>
+        <div className="ml-[4px]">
+          <p className="text-[18px] font-medium text-gray-800 mt-[8px]">
             지나가는 나그네{name}
           </p>
-          <div className='flex'>
+          <div className="flex">
             <Image
               src={'/assets/images/mail.svg'}
-              alt='Mail Icon'
+              alt="Mail Icon"
               width={11}
               height={9}
             />
-            <p className='text-[12px] text-gray-500 m-1'>
+            <p className="text-[12px] text-gray-500 m-1">
               rhdiddl@gmail.com{email}
             </p>
+            <ClipboardButton textToCopy={emailText} />
           </div>
         </div>
       </div>
-      <p className='text-sm text-gray-700 my-4'>
+      <p className="text-sm text-gray-700 my-4">
         손 내밀면 콕하고 찔릴 거야 네모 같은 우리 서로 남 탓하는 건 (예나
         지금이나 그대로) 상처투성이야 이러다 나 죽어 네모나고 모난 우린
         삐뚤빼뚤해 like
       </p>
 
       <button
-        className='px-4 py-2 w-[700px] h-[61px] mt-2 text-[18px] font-medium border rounded-md'
+        className="px-4 py-2 w-[700px] h-[61px] mt-2 text-[18px] font-medium border rounded-md"
         style={{
           borderColor: '#FBA250',
           backgroundColor: '#FFFFFF',
