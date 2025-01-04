70709c59cc5459cfb5314e22bfc92503e01c9ad5
seorinnn <srgongju@naver.com>
2025-01-04 16:44:10 +0900

    chore : 이미지 도메인 추가 및 백엔드 경로 변경

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,14 @@ const withSvgr = require('next-svgr');
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // React의 엄격 모드 활성화
+  images: {
+    domains: ['lh3.googleusercontent.com'], // 이미지 도메인 추가
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*', // 클라이언트가 요청하는 경로
-        destination: 'http://43.200.18.88:8080/api/:path*', // 프록시할 백엔드 경로
+        destination: 'https://api.dreamketcher.site/api/:path*', // 프록시할 백엔드 경로
       },
     ];
   },
