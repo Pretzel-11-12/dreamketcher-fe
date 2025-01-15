aae980f19050d66d09f44d87a4d16869ee38fafb
김성현 <skyladder8895@gmail.com>
2025-01-15 18:03:13 +0900

    style: change global font to pretendard

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../static/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr" className={`${pretendard.variable}`}>
+      <body className={pretendard.className}>{children}</body>
+    </html>
+  );
+}
