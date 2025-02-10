183dc50087f49bcc235e49ae7bf5268849a9d3c5
김성현 <skyladder8895@gmail.com>
2025-02-10 22:30:47 +0900

    style: add shadow to quickmenu

--- src/app/(default)/main/_component/QuickMenu.tsx
+++ src/app/(default)/main/_component/QuickMenu.tsx
@@ -9,7 +9,7 @@ const QuickMenu = () => {
   return (
     <div className="fixed flex z-50 right-[30px] top-[625px]">
       <div
-        className="cursor-pointer rounded-full border bg-white w-[50px] h-[50px] flex items-center justify-center  text-gray-600/50 hover:text-gray-600"
+        className="cursor-pointer rounded-full shadow-[0_0_4px_0px_rgba(0,0,0,0.25)] bg-white w-[50px] h-[50px] flex items-center justify-center"
         onClick={handleClickTop}
       >
         <Image
