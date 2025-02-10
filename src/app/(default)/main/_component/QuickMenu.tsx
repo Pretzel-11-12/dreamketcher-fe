42a48d3c7c888d4221459f319399829614332781
김성현 <skyladder8895@gmail.com>
2025-02-10 22:23:49 +0900

    style: change floating button style

--- src/app/(default)/main/_component/QuickMenu.tsx
+++ src/app/(default)/main/_component/QuickMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 import _ from 'lodash';
-
+import Image from 'next/image';
 const QuickMenu = () => {
   const handleClickTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -12,7 +12,12 @@ const QuickMenu = () => {
         className="cursor-pointer rounded-full border bg-white w-[50px] h-[50px] flex items-center justify-center  text-gray-600/50 hover:text-gray-600"
         onClick={handleClickTop}
       >
-        <span className="mdi mdi-chevron-up text-2xl"></span>
+        <Image
+          src={'/assets/icon/arrow-up.svg'}
+          alt={'위로 이동 이미지'}
+          width={24}
+          height={24}
+        />
       </div>
     </div>
   );
