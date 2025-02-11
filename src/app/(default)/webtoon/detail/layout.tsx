0f20bc6e2486857e57023c577ca8d6296df11c3a
seorinnn <srgongju@naver.com>
2025-02-11 22:15:46 +0900

    style : 웹툰 상세페이지 layout 디자인 변경

--- src/app/(default)/webtoon/detail/layout.tsx
+++ src/app/(default)/webtoon/detail/layout.tsx
@@ -8,7 +8,7 @@ type Props = { children: ReactNode };
 export default function DetailLayout({ children }: Props) {
   return (
     <>
-      <div className="pt-[80px] pb-10">
+      <div className="pt-[70px] pb-10">
         <QuickMenu />
 
         <div className="w-full flex flex-col items-center bg-white text-black">
