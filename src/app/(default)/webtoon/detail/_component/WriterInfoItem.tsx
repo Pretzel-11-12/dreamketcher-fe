997a70ee75ce58b91bcb821d6fa27b92f4d1c305
seorinnn <srgongju@naver.com>
2025-02-20 14:46:45 +0900

    style : 에피소드 페이지 디자인 수정

--- src/app/(default)/webtoon/detail/_component/WriterInfoItem.tsx
+++ src/app/(default)/webtoon/detail/_component/WriterInfoItem.tsx
@@ -9,7 +9,7 @@ export default function WriterInfoItem({ name, story, authorImage }: UserInfo) {
   const src = authorImage || '/assets/images/profile-default.png';
 
   return (
-    <div className="flex flex-col gap-1.5">
+    <div className="flex flex-col">
       <div className="grid grid-cols-[auto_1fr] items-center gap-2">
         <Image
           src={src}
@@ -23,7 +23,7 @@ export default function WriterInfoItem({ name, story, authorImage }: UserInfo) {
           <div className="text-gray-400 text-sm">글 / 그림</div>
         </div>
       </div>
-      <div className="text-sm px-0.5 pt-1.5">{story}</div>
+      <div className="text-[15px]/[18px] pt-[10px]">{story}</div>
     </div>
   );
 }
