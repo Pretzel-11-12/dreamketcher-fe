ebf2ff1b6ef6139851a165cacc2790250c5df688
seorinnn <srgongju@naver.com>
2025-03-09 00:54:30 +0900

    feat : 댓글 추천, 임시 댓글 상태 기능 추가

--- src/app/(default)/webtoon/detail/_component/CommentItem.tsx
+++ src/app/(default)/webtoon/detail/_component/CommentItem.tsx
@@ -22,6 +22,8 @@ const CommentItem: React.FC<CommentInfoType> = ({
   const timeAgo = moment.utc(info.createdAt).tz('Asia/Seoul').fromNow();
 
   const [showMenu, setShowMenu] = useState(false); // 메뉴 상태
+  const [isLiked, setIsLiked] = useState(false); // 좋아요 상태
+  const [isDisliked, setIsDisliked] = useState(false); // 싫어요 상태
   const menuRef = useRef<HTMLDivElement>(null); // 메뉴 참조
 
   const toggleMenu = () => {
@@ -44,6 +46,67 @@ const CommentItem: React.FC<CommentInfoType> = ({
 
   const queryClient = useQueryClient();
 
+  // 댓글 추천
+  const { mutate: recommendCommentMutate } = useMutation({
+    mutationFn: (variables: {
+      webtoonId: string;
+      episodeId: string;
+      commentId: string;
+    }) =>
+      fetchComment.recommendComment(
+        variables.webtoonId,
+        variables.episodeId,
+        variables.commentId
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [webtoonId, episodeId, 'comments'],
+      });
+    },
+    onError: (e) => console.log(e),
+  });
+
+  // 댓글 추천 취소
+  const { mutate: deleteRecommendCommentMutate } = useMutation({
+    mutationFn: (variables: {
+      webtoonId: string;
+      episodeId: string;
+      commentId: string;
+    }) =>
+      fetchComment.deleteRecommendComment(
+        variables.webtoonId,
+        variables.episodeId,
+        variables.commentId
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [webtoonId, episodeId, 'comments'],
+      });
+    },
+    onError: (e) => console.log(e),
+  });
+
+  // 댓글 비추천
+  const { mutate: notRecommendCommentMutate } = useMutation({
+    mutationFn: (variables: {
+      webtoonId: string;
+      episodeId: string;
+      commentId: string;
+    }) =>
+      fetchComment.notRecommendComment(
+        variables.webtoonId,
+        variables.episodeId,
+        variables.commentId
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [webtoonId, episodeId, 'comments'],
+      });
+    },
+    onError: (e) => console.log(e),
+  });
+
+  // 댓글 삭제
   const { mutate: deleteCommentMutate, isError } = useMutation({
     mutationFn: fetchComment.deleteComment,
     onSuccess: () => {
@@ -63,6 +126,43 @@ const CommentItem: React.FC<CommentInfoType> = ({
     },
   });
 
+  // 좋아요/싫어요 상태 관리
+  const toggleLike = () => {
+    if (isLiked) {
+      deleteRecommendCommentMutate({
+        webtoonId,
+        episodeId,
+        commentId: String(info.id),
+      });
+      setIsLiked(false);
+    } else {
+      recommendCommentMutate({
+        webtoonId,
+        episodeId,
+        commentId: String(info.id),
+      });
+      setIsLiked(true);
+    }
+  };
+
+  const toggleDislike = () => {
+    if (isDisliked) {
+      notRecommendCommentMutate({
+        webtoonId,
+        episodeId,
+        commentId: String(info.id),
+      });
+      setIsDisliked(false);
+    } else {
+      notRecommendCommentMutate({
+        webtoonId,
+        episodeId,
+        commentId: String(info.id),
+      });
+      setIsDisliked(true);
+    }
+  };
+
   return (
     <div className="grid grid-cols-[auto_1fr] py-5 gap-[14px] border-b border-gray-500/10">
       <Image
@@ -70,15 +170,17 @@ const CommentItem: React.FC<CommentInfoType> = ({
         alt="profile"
         width={36}
         height={36}
-        className="w-9 h-9 rounded-full object-cover shadow-[inset_0_0_0_1px_#F2F2F2]"
+        className="w-9 h-9 rounded-full border border-[#F2F2F2] object-cover shadow-[inset_0_0_0_1px_#F2F2F2]"
       />
       <div className="flex flex-col gap-2">
         <div className="grid grid-cols-[1fr_auto] items-center">
-          <div className="text-sm/[17px]">{info.nickname}</div>
+          <div className="text-sm/[17px] text-[#282828] font-semibold">
+            {info.nickname}
+          </div>
           <div className="text-xs text-[#888888]">{timeAgo}</div>
         </div>
 
-        <div className="text-[13px]">{info.content}</div>
+        <div className="text-[13px] text-[#3F3F3F]">{info.content}</div>
         <div className="h-5 mt-1 flex gap-4 text-[#888888]">
           <div
             className="text-xs flex items-center gap-1 cursor-pointer"
@@ -93,23 +195,45 @@ const CommentItem: React.FC<CommentInfoType> = ({
             {info.childCommentCount > 0 ? info.childCommentCount : '답글'}
           </div>
 
-          <div className="text-xs flex items-center gap-1 cursor-pointer">
+          <div
+            className="text-xs flex items-center gap-1 cursor-pointer"
+            onClick={toggleLike}
+          >
             <Image
-              src="/assets/icon/inactiveLike.svg"
+              src={
+                isLiked
+                  ? '/assets/icon/activeLike.svg'
+                  : '/assets/icon/inactiveLike.svg'
+              }
               alt="like"
               width={13}
               height={13}
             />
-            좋아요
+            <span className={isLiked ? 'text-brand-yellow' : ''}>
+              {info.recommendationCount > 0
+                ? info.recommendationCount
+                : '좋아요'}
+            </span>
           </div>
-          <div className="text-xs flex items-center gap-1 cursor-pointer">
+          <div
+            className="text-xs flex items-center gap-1 cursor-pointer"
+            onClick={toggleDislike}
+          >
             <Image
-              src="/assets/icon/inactiveDislike.svg"
+              src={
+                isDisliked
+                  ? '/assets/icon/activeDislike.svg'
+                  : '/assets/icon/inactiveDislike.svg'
+              }
               alt="dislike"
               width={13}
               height={13}
             />
-            싫어요
+            <span className={isDisliked ? 'text-[#2E4072]' : ''}>
+              {info.notRecommendationCount > 0
+                ? info.notRecommendationCount
+                : '싫어요'}
+            </span>
           </div>
           <div className="flex relative ml-auto" ref={menuRef}>
             <Image
