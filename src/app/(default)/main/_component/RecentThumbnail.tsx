ab9e1f5cbae23aecf65cc69625fd46eb5f6b0739
김성현 <skyladder8895@gmail.com>
2025-03-19 09:28:27 +0900

    style: update RecentThumbnail gap

--- src/app/(default)/main/_component/RecentThumbnail.tsx
+++ src/app/(default)/main/_component/RecentThumbnail.tsx
@@ -63,7 +63,7 @@ const RecentThumbnail: React.FC<RecentThumbnailProps> = ({ webtoon }) => {
         </p>
       </div>
       <Link
-        className="w-[138px] h-[43px] mt-[12px] flex items-center justify-center bg-brand-yellow text-white text-[14px] rounded-[5px]"
+        className="w-[138px] h-[43px] mt-[6px] flex items-center justify-center bg-brand-yellow text-white text-[14px] rounded-[5px]"
         href={`/webtoon/detail?titleId=${webtoon.id}&no=${webtoon.episodeCount}`}
       >
         {webtoon.episodeCount}화 이어읽기
