86cf746c9665c9c14416298fe0f9ffeaf2c662d1
seorinnn <srgongju@naver.com>
2025-04-23 21:12:56 +0900

    feat : Loading 컴포넌트 lottielab 에서 Image 태그 gif 사용으로 변경

--- src/app/_component/Loading.tsx
+++ src/app/_component/Loading.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import React from 'react';
+import Image from 'next/image';
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <iframe
-        src="https://cdn.lottielab.com/l/5wfqqtdnsd3NSF.html"
-        width="200"
-        height="200"
-        frameBorder="0"
-        allowFullScreen
-        title="loading"
+    <div className="flex items-center justify-center h-screen">
+      <Image
+        src="/assets/icon/Loading.gif"
+        alt="로딩 중"
+        width={100}
+        height={100}
       />
     </div>
   );
