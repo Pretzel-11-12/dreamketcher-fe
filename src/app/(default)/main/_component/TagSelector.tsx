a8b80d112cc6135016a5f9f55776ace9e4e8ba36
boozeal <skyladder8895@gmail.com>
2025-05-21 10:04:07 +0900

    style: Change TagSelector button border & background colors

--- src/app/(default)/main/_component/TagSelector.tsx
+++ src/app/(default)/main/_component/TagSelector.tsx
@@ -20,8 +20,8 @@ const TagSelector: React.FC<TagSelectorProps> = ({
           key={tag.name}
           className={`flex items-center justify-center h-[26px] text-[13px] rounded-[5px] px-[10px] transition duration-300 ${
             selectedTag === tag.name
-              ? 'bg-[#5474CE] text-white'
-              : 'bg-[#f2f2f2] text-[#888888] hover:bg-[#5474CE] hover:text-white'
+              ? 'bg-[#5474CE] border border-[#4C68B4] text-white'
+              : 'bg-[#f2f2f2] text-[#888888] hover:bg-[#E9E9E9] border border-[#F3F0F0] hover:border-[#E0E0E0]'
           }`}
           onClick={() => handleTagClick(tag.name)}
         >
