592d1c7ac811ace964846ed498f417c51039a57f
seorinnn <srgongju@naver.com>
2025-05-31 23:43:17 +0900

    feat : 책장 목록에 페이지네이션 추가

--- src/app/(default)/mypage/_component/BookShelf/BookShelf.tsx
+++ src/app/(default)/mypage/_component/BookShelf/BookShelf.tsx
@@ -5,14 +5,25 @@ import BookShelfItem from './BookShelfItem';
 import { useQuery } from '@tanstack/react-query';
 import { getBookShelfFolder } from '@/app/api/fetchFolder';
 import { Folder } from '@/app/api/fetchFolder/model';
+import Pagination from '@/app/_component/Pagination';
 
 const BookShelf: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 11;
 
   const { data: bookShelves = [], isLoading, isError } = useQuery<Folder[]>({
     queryKey: ['bookShelves'],
     queryFn: getBookShelfFolder,
   });
+
+  const totalPages = Math.ceil(bookShelves.length / itemsPerPage);
+
+  // 현재 페이지에 해당하는 책장들만 슬라이스
+  const currentBookShelves = bookShelves.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   const handleAddBookShelf = () => {
     setIsModalOpen(true);
@@ -22,6 +33,10 @@ const BookShelf: React.FC = () => {
     setIsModalOpen(false);
   };
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="mt-5">
       <div className="grid grid-cols-4 gap-2.5 mb-4">
@@ -29,7 +44,7 @@ const BookShelf: React.FC = () => {
           <BookShelfAddButton onClick={handleAddBookShelf} />
         </div>
         {/* 첫 번째 줄에 3개의 책장이 배치 */}
-        {bookShelves.slice(0, 3).map((shelf, index) => (
+        {currentBookShelves.slice(0, 3).map((shelf, index) => (
           <div key={index} className="col-span-1">
             <BookShelfItem shelf={shelf} />
           </div>
@@ -38,16 +53,23 @@ const BookShelf: React.FC = () => {
 
       {/* 그 이후 줄은 4개씩 배치 */}
       <div className="grid grid-cols-4 gap-2.5">
-        {bookShelves.slice(3).map((shelf, index) => (
+        {currentBookShelves.slice(3).map((shelf, index) => (
           <div key={index} className="col-span-1">
             <BookShelfItem shelf={shelf} />
           </div>
         ))}
       </div>
 
-      <BookShelfAddModal isOpen={isModalOpen} onClose={closeModal}/>
-    </div>
+      <div className="mt-5">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
 
+      <BookShelfAddModal isOpen={isModalOpen} onClose={closeModal} />
+    </div>
   );
 };
 
