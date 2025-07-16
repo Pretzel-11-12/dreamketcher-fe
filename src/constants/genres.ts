cf8bc8b442462e8524b637a89ee8cfed77e32f67
boozeal <skyladder8895@gmail.com>
2025-07-16 14:33:47 +0900

    fix: Change genre constant

--- src/constants/genres.ts
+++ src/constants/genres.ts
@@ -1,11 +1,12 @@
 export const genres: { name: string; param: string }[] = [
-  { name: '학원물', param: 'SCHOOLLIFE' },
-  { name: '판타지', param: 'FANTASY' },
   { name: '로맨스', param: 'ROMANCE' },
+  { name: '판타지', param: 'FANTASY' },
   { name: '무협', param: 'MARTIALARTS' },
-  { name: '일상물', param: 'SLICEOFLIFE' },
+  { name: '일상', param: 'SLICEOFLIFE' },
   { name: '스릴러', param: 'THRILLER' },
-  { name: '역사물', param: 'HISTORICAL' },
+  { name: '액션', param: 'ACTION' },
   { name: '스포츠', param: 'SPORTS' },
-  { name: '코믹', param: 'COMEDY' },
+  { name: '개그', param: 'COMEDY' },
+  { name: '소년', param: 'YOUNG' },
+  { name: '공포', param: 'HORROR' },
 ];
